e6a3042e492a6168267e9cd6fe4a9904289164bf
ShalbaJohnes <wawerudebbie@gmail.com>
2025-10-31 10:27:33 +0300

    modified changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
-
 import React from "react";
 
-function Links({ github, linkedin }) {
+function NavBar() {
   return (
-    <div>
-      <h3>Links</h3>
-      <a href={github}>{github}</a>
-      <a href={linkedin}>{linkedin}</a>
-    </div>
+    <nav>
+      <a href="#home">home</a>
+      <a href="#about">about</a>
+      <a href="#projects">projects</a>
+    </nav>
   );
 }
 
-export default Links;
+export default NavBar;
